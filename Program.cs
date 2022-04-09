1d321ca76172140203c29f6cfff49d11f3f55a07
leeool <lgonsalez02@gmail.com>
2022-04-08 21:48:26 -0300

    finaliza exericico

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿double n1, n2;
+
+Console.Write("Digite o primeiro número: ");
+n1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o segundo número: ");
+n2 = Convert.ToDouble(Console.ReadLine());
+
+if (n1 > n2){
+    Console.WriteLine($"O maior número é {n1}");
+}
+else{
+    Console.WriteLine($"O maior numero é {n2}");
+}
+
+
